7fc80f129462618231c58e15ad30add74a00db1f
Marion.L <mrn.larue@gmail.com>
2026-03-24 14:38:18 +0100

    Update main.py

--- Exercice08/main.py
+++ Exercice08/main.py
@@ -1,8 +1,15 @@
 def log_decorator(func):
-     pass
- 
+    def wrapper():
+        print("Avant l'exécution")
+        func()
+        print("Après l'exécution")
+
+    return wrapper
+
+
 @log_decorator
 def function_test():
     print("Cette fonction ne prend pas d'arguments.")
+
 
 function_test()
