5c5c7508d8161babf691ffb79c6893707fed8ce1
Marion.L <mrn.larue@gmail.com>
2026-03-24 14:47:10 +0100

    Update main.py

--- Exercice12/main.py
+++ Exercice12/main.py
@@ -1,11 +1,49 @@
 class Book:
     def __init__(self, title, author, year):
+        # Infos du livre
         self.title = title
         self.author = author
         self.year = year
 
+
 class Library:
     def __init__(self):
+        # Livres dispo
         self.books = []
-	self.borrow_books = []
-    # Ajouter les méthodes ici
+        # Livres empruntés
+        self.borrowed_books = []
+
+    def add_book(self, book):
+        # Ajoute un livre à la bibliothèque
+        self.books.append(book)
+
+    def remove_book(self, book_title):
+        # Supprime un livre par son titre
+        for book in self.books:
+            if book.title == book_title:
+                self.books.remove(book)
+                break
+
+    def borrow_book(self, book_title):
+        # Déplace un livre de books vers borrowed_books
+        for book in self.books:
+            if book.title == book_title:
+                self.books.remove(book)
+                self.borrowed_books.append(book)
+                break
+
+    def return_book(self, book_title):
+        # Remet un livre emprunté dans les livres dispo
+        for book in self.borrowed_books:
+            if book.title == book_title:
+                self.borrowed_books.remove(book)
+                self.books.append(book)
+                break
+
+    def available_books(self):
+        # Retourne les titres des livres disponibles
+        return [book.title for book in self.books]
+
+    def borrowed_books_list(self):
+        # Retourne les titres des livres empruntés
+        return [book.title for book in self.borrowed_books]
