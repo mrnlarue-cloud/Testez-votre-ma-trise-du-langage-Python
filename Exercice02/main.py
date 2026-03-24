1874b37b44e6cf671a9ba19c1b2092bf6962c6a1
Marion.L <mrn.larue@gmail.com>
2026-03-24 14:34:38 +0100

    Update main.py

--- Exercice02/main.py
+++ Exercice02/main.py
@@ -1,17 +1,21 @@
 students = {
-    'Alice': {
-         'Mathematiques': 90,
-         'Francais': 80,
-         'Histoire': 95
-    },
-    'Bob': {
-         'Mathematiques': 75,
-         'Francais': 85,
-         'Histoire': 70
-    },
-     'Charlie': {
-         'Mathematiques': 88,
-         'Francais': 92,
-         'Histoire': 78
-     }
+    "Alice": {"Mathematiques": 90, "Francais": 80, "Histoire": 95},
+    "Bob": {"Mathematiques": 75, "Francais": 85, "Histoire": 70},
+    "Charlie": {"Mathematiques": 88, "Francais": 92, "Histoire": 78},
 }
+
+
+name = input("Entrez le nom de l’étudiant : ")
+
+
+if name in students:
+    notes = students[name]
+    print(f"Notes de {name} :")
+
+    for matiere, note in notes.items():
+        print(f"{matiere} : {note}")
+
+    moyenne = sum(notes.values()) / len(notes)
+    print(f"Moyenne de {name} : {moyenne:.2f}")
+else:
+    print(f"L'étudiant {name} n'existe pas dans la liste.")
