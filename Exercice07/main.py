48dcbf4a95fef408d4db31657b69e1b16f07b044
Marion.L <mrn.larue@gmail.com>
2026-03-24 14:37:47 +0100

    Update main.py

--- Exercice07/main.py
+++ Exercice07/main.py
@@ -1,1 +1,9 @@
-## Écrivez votre code ici !
+def square(number):
+    # Vérifie si la valeur donnée est bien un nombre
+    if not isinstance(number, (int, float)):
+        # Affiche un message d'erreur si ce n'est pas un nombre
+        print("Le paramètre doit être un nombre !")
+        return None
+
+    # Retourne le carré du nombre
+    return number ** 2
