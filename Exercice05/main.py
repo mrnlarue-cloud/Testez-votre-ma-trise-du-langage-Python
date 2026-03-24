575534fb2a15f151c19a4e96d5da013559dbec35
Marion.L <mrn.larue@gmail.com>
2026-03-24 14:36:25 +0100

    Update main.py

--- Exercice05/main.py
+++ Exercice05/main.py
@@ -1,5 +1,24 @@
-def sum(a, b):
+def SUM(a, b):
+    """Calcule la somme de deux nombres.
+
+    Paramètres :
+    a : premier nombre
+    b : deuxième nombre
+
+    Retourne :
+    la somme de a et b
+    """
     return a + b
- 
+
+
 def subtraction(a, b):
+    """Calcule la différence entre deux nombres.
+
+    Paramètres :
+    a : premier nombre
+    b : deuxième nombre
+
+    Retourne :
+    la différence entre a et b
+    """
     return a - b
