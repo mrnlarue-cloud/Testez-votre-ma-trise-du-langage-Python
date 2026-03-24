5ccf8004f05ff80e26fa855f5cbc865befb8894e
Marion.L <mrn.larue@gmail.com>
2026-03-24 14:33:24 +0100

    Update main.py

--- Exercice11/main.py
+++ Exercice11/main.py
@@ -1,1 +1,19 @@
-## Écrivez votre code ici !
+class BankAccount:
+    def __init__(self, account_holder, balance):
+        self.account_holder = account_holder
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+        else:
+            print("Fonds insuffisants")
+        return self
+
+    def display_balance(self):
+        print(f"{self.account_holder} : {self.balance}")
+        return self
