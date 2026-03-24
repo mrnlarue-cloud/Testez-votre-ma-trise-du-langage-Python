3015083a9e822ccbc2fd23a6b2371c90d712e8d3
Marion.L <mrn.larue@gmail.com>
2026-03-24 14:35:08 +0100

    Update main.py

--- Exercice03/main.py
+++ Exercice03/main.py
@@ -1,1 +1,14 @@
 words = ["python", "programmation", "langage", "ordinateur", "apprentissage"]
+
+voyelles = "aeiouy"
+
+resultats = []
+
+for mot in words:
+    count = 0
+    for lettre in mot:
+        if lettre in voyelles:
+            count += 1
+    resultats.append((mot, count))
+
+print(resultats)
